dec4e6435430aaf43f1953cc7c56d34a9f7fce95
PERRUCCI ISRAEL <B040825@rio.ar.bsch>
2017-10-25 17:10:16 -0300

    Encrypt Password

--- models/user.js
+++ models/user.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
+//Create UserSchema
 const userSchema = new Schema({
   email: { type: 'string', unique: true, lowercase: true },
   password: 'string'
 });
-// Create the model class
+
+//Save password
+userSchema.pre('save', function(next) {
+  const user = this;
+  //Create a salt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    //Create a hash
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+//Create UserModel
 const UserModel = mongoose.model('user', userSchema);
 module.exports = UserModel;
