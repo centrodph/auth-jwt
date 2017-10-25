9023f23f991412bfb79652febbded8a120586ac4
PERRUCCI ISRAEL <B040825@rio.ar.bsch>
2017-10-25 16:24:23 -0300

    Check if user exist

--- controllers/signup.js
+++ controllers/signup.js
@@ -4,6 +4,12 @@ module.exports = function(req, res) {
   const email = req.body.email;
   const password = req.body.password;
   const user = new User({ email: email, password: password });
-  user.save();
-  res.send({ signup: 'working', email: email, password: password });
+  User.findOne({ email: email }, (err, existingUser) => {
+    if (err) return res.status(422).send(err.errmsg);
+    if (existingUser) return res.status(422).send('Email in use');
+    user.save(function(err, user) {
+      if (err) return res.status(422).send(err.errmsg);
+      return res.send(user);
+    });
+  });
 };
