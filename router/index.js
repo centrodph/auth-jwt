20ed4e6cc275be3fa110ef36849fe1faf481156d
GerardoMac <centrodph@gmail.com>
2017-11-03 07:22:39 -0300

    Expose routes

--- router/index.js
+++ router/index.js
@@ -7,11 +7,13 @@ const requireAuthLocal = passport.authenticate('local', { session: false });
 module.exports = function(app) {
   //Home Public
   app.get('/', function(req, res) {
-    res.send({ api: {
-      "/secret" : "requireAuth TOKEN",
-      "/signin" : "requireAuthLocal email and password",
-      "/signup" : "create a user with email and password"
-    }} });
+    res.send({
+      api: {
+        '/secret': 'requireAuth TOKEN',
+        '/signin': 'requireAuthLocal email and password',
+        '/signup': 'create a user with email and password'
+      }
+    });
   });
 
   //Require TOKEN
